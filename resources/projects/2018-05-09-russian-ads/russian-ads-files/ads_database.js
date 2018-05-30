32419c132ca1300ab099c4c8c67d43d17b0a4eed
AndrewBeers <Andrew_Beers@alumni.brown.edu>
2018-05-30 00:09:50 -0400

    ra: JS linking problem

--- resources/projects/2018-05-09-russian-ads/russian-ads-files/ads_database.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:767ababd03fddf42606ccf9bcc3276c46c7c145ee9c906c3e18521de857b7e63
-size 4148686
---
+++ resources/projects/2018-05-09-russian-ads/russian-ads-files/russian_ads.js
@@ -0,0 +1,4 @@
+var russian_ads;
+$.getJSON('https://media.githubusercontent.com/media/AndrewBeers/anderff-site/master/resources/projects/2018-05-09-russian-ads/russian-ads-files/ads_database.json', function(json) {
+  russian_ads = json;
+});
