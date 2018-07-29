cd1f3333a830c664aba3c99ae0254e9c978e2968
AndrewBeers <Andrew_Beers@alumni.brown.edu>
2018-07-29 19:11:02 -0400

    General site update

--- resources/projects/2018-05-09-russian-ads/Ad_Gallery.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $.getJSON("/resources/projects/2018-05-09-russian-ads/russian_ads.json",function(data){
-       $.each(data, function (index, value) {
-         console.log(value['Ad ID']);
-         var content = '<h3>' + value['Ad ID'] + '</h3>';
-          $(content).appendTo("#ads_list");       // READ THE DATA.
-       });
-    });  
-});
